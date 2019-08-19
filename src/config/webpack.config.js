bdc7d16526e757f0c45cb75536d91b660de615c6
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2019-08-19 08:09:14 -0600

    update deps

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -1,12 +1,10 @@
 const externals = require("webpack-node-externals")
 
-const isLocal = slsw.lib.webpack.isLocal
-
 module.exports = slsw => ({
   devtool: "source-map",
   entry: slsw.lib.entries,
   externals: [externals()],
-  mode: isLocal ? "development" : "production",
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
   module: { rules: [{ test: /\.tsx?$/, loader: "ts-loader" }] },
   performance: { hints: false },
   resolve: { extensions: [".js", ".jsx", ".json", ".ts", ".tsx"] },
