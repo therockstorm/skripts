cb7841a6e34fd616245fa8dd9e2af58bee66cd40
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-01-30 16:29:57 -0700

    Fix webpack.config

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -1,15 +1,9 @@
-const webpack = require("webpack")
-
-module.exports = (async (slsw) => {
-  const accountId = await slsw.lib.serverless.providers.aws.getAccountId()
-  return {
-    devtool: "source-map",
-    entry: slsw.lib.entries,
-    mode: slsw.lib.webpack.isLocal ? "development" : "production",
-    module: { rules: [{ test: /\.tsx?$/, loader: "ts-loader" }] },
-    performance: { hints: false },
-    plugins: [new webpack.DefinePlugin({ AWS_ACCOUNT_ID: `${accountId}` })],
-    resolve: { extensions: [".js", ".jsx", ".json", ".ts", ".tsx"] },
-    target: "node",
-  }
-})()
+module.exports = (slsw) => ({
+  devtool: "source-map",
+  entry: slsw.lib.entries,
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  module: { rules: [{ test: /\.tsx?$/, loader: "ts-loader" }] },
+  performance: { hints: false },
+  resolve: { extensions: [".js", ".jsx", ".json", ".ts", ".tsx"] },
+  target: "node",
+})
