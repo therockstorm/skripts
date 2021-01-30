fc21fc85f2b69632506681e212ea8f774dd30a7a
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-01-30 16:31:15 -0700

    remove git add

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -3,9 +3,6 @@ const { resolveSelf } = require("../utils")
 const self = resolveSelf()
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [`${self} lint`, `${self} format`, "git add"],
-  "*.{json,yml,yaml,md,html,css,less,scss,graphql}": [
-    `${self} format`,
-    "git add",
-  ],
+  "*.{js,jsx,ts,tsx}": [`${self} lint`, `${self} format`],
+  "*.{json,yml,yaml,md,html,css,less,scss,graphql}": [`${self} format`],
 }
